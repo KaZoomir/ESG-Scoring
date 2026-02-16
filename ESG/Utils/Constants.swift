3bdc1302aece15936195b27d1602e0e54c2d7a1c
KaZoomir <kazoomir@gmail.com>
2026-02-16 22:30:50 +0500

    Fix bag

--- ESG/Utils/Constants.swift
+++ ESG/Utils/Constants.swift
@@ -36,7 +36,7 @@ extension Color{
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         var int: UInt64 = 0
         Scanner (string: hex).scanHexInt64(&int)
-        let a,r,g,b: Int64
+        let a,r,g,b: UInt64
         switch hex.count{
         case 3:
             (a,r,g,b) = (255,(int >> 8 ) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
